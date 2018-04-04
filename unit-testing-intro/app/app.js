f7a58e1c29b0d006ce93d6c058ec8856902f7f80
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2018-04-04 15:16:22 +0200

    Jasmine in tests

---
+++ unit-testing-intro/app/app.js
@@ -0,0 +1,14 @@
+var codes = {
+  'Poland': 48,
+  'Spain': 34,
+  'Germany': 49,
+}
+
+exports.findPrefixCode = function (country) {
+  if (country in codes) {
+    return codes[country]
+  }
+  else {
+    return 'Unknown country';
+  }
+}
